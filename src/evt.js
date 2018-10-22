c654696f901212d3a55ace43a048a7161c8616c4
huangsiyue <316689987@qq.com>
2018-10-22 10:40:55 +0800

    update files of evt

--- src/evt.js
+++ src/evt.js
@@ -1,165 +1,184 @@
-const Utils = {
-	type(o) {
-		return Object.prototype.toString.call(o).slice(8, -1).toLowerCase();
-	}
-};
+import utils from './utils';
 
-export class FnQueue {
-	constructor(options) {
-		this.queue = [];
-		this.stack = [];
-		this.add = this.add.bind(this);
-		this.next = this.next.bind(this);
-		this.skip = this.skip.bind(this);
-		this.fire = this.fire.bind(this);
-		this.clear = this.clear.bind(this);
-	}
+class FnQueue {
+  constructor() {
+    this.options = {
+      // 是否自动执行
+      autoExcute: false
+    };
+    this.queue = [];
+    this.cursor = 0;
+    this.add = this.add.bind(this);
+    this.next = this.next.bind(this);
+    this.skip = this.skip.bind(this);
+    this.fire = this.fire.bind(this);
+    this.clear = this.clear.bind(this);
+  }
 
-	add(fns) {
-		switch (Utils.type(fns)) {
-			case 'function':
-				this.queue.push(fns);
-				this.stack.push(fns);
-				break;
-			case 'array':
-				fns.map(fn => {
-					this.add(fn);
-				});
-				break;
-			default:
-				return;
-		}
-		return this;
-	}
+  add(fns, options = {}) {
+    let { token } = Object.assign(this.options, options);
 
-	skip(step) {
-		step = Number(step) || 1;
-		while (step--) {
-			this.queue.shift();
-		}
-		return this;
-	}
+    switch (utils.type(fns)) {
+      case 'function':
+        fns.token = token;
+        this.queue.push(fns);
+        break;
+      case 'array':
+        fns.map(fn => {
+          this.add(fn);
+        });
+        break;
+      default:
+        return;
+    }
+    return this;
+  }
 
-	cycle() {
-		this.queue = Object.assign([], this.stack);
-		return this;
-	}
+  skip(step) {
+    step = utils.isNull(step) ? 1 : +step;
+    while (step--) {
+      this.queue.shift();
+    }
+    return this;
+  }
 
-	next() {
-		Array.prototype.unshift.call(arguments, this);
-		return this.queue.shift().apply(undefined, arguments);
-	}
+  next(...args) {
+    const {
+      options: { autoExcute }
+    } = this;
+    if (utils.notNull(this.queue)) {
+      // 取出队列第一个元素
+      const fn = this.queue.shift();
+      // 游标+1
+      this.cursor++;
+      // 开始执行
+      fn.apply(null, args);
+      // 执行之后添加到队列尾部
+      this.queue.push(fn);
+      if (this.cursor < this.queue.length && autoExcute) {
+        this.next(...args);
+      } else {
+        this.cursor = 0;
+      }
+    }
+  }
 
-	fire() {
-		return this.next(...arguments);
-	}
+  fire(...args) {
+    this.cursor = 0;
+    return this.next(...args);
+  }
 
-	clear() {
-		this.queue = this.stack = [];
-		return this;
-	}
+  clear() {
+    this.queue = [];
+    return this;
+  }
 
-	remove(ids) {
-		const type = Utils.type(ids);
+  remove(ids) {
+    const type = utils.type(ids);
 
-		if (!/function|string|array|undefined/.test(type)) {
-			return false;
-		}
+    if (!/function|string|array|undefined/.test(type)) {
+      return false;
+    }
 
-		// 移除订阅内容
-		switch (type) {
-			case 'undefined': {
-				this.queue = [];
-				break;
-			}
-			case 'array': {
-				this.queue = this.queue.filter(fn => {
-					return !ids.includes(fn) && !ids.includes(fn.name);
-				});
-				break;
-			}
-			case 'function': {
-				this.queue = this.queue.filter(fn => {
-					return ids !== fn;
-				});
-				break;
-			}
-			case 'string': {
-				this.queue = this.queue.filter(fn => {
-					return ids !== fn.name;
-				});
-				break;
-			}
-		}
+    // 移除订阅内容
+    switch (type) {
+      case 'undefined': {
+        this.queue = [];
+        break;
+      }
+      case 'array': {
+        this.queue = this.queue.filter(fn => {
+          return !ids.includes(fn) && !ids.includes(fn.name) && !ids.includes(fn.token);
+        });
+        break;
+        F;
+      }
+      case 'function': {
+        this.queue = this.queue.filter(fn => {
+          return ids !== fn;
+        });
+        break;
+      }
+      case 'string': {
+        this.queue = this.queue.filter(fn => {
+          return ids !== fn.name && ids !== fn.token;
+        });
+        break;
+      }
+    }
 
-		return this;
-	}
+    return this;
+  }
 }
 
-export class Signal {
-	constructor() {
-		this.stack = {};
-		this.cache = {};
-		this.add = this.add.bind(this);
-		this.on = this.on.bind(this);
-		this.emit = this.emit.bind(this);
-		this.remove = this.remove.bind(this);
-	}
+/* 发布订阅 */
+class Signal {
+  constructor(props) {
+    this.options = {
+      // 是否自动执行
+      autoExcute: true
+    };
+    this.stack = {};
+    this.cache = {};
+    this.add = this.add.bind(this);
+    this.on = this.on.bind(this);
+    this.emit = this.emit.bind(this);
+    this.remove = this.remove.bind(this);
+  }
 
-	// 添加到目标列表
-	add(queue, fns) {
-		queue = queue && queue.add ? queue : new FnQueue();
-		queue.add(fns);
-		return queue;
-	}
+  // 添加到目标列表
+  add(queue, fns = [], options) {
+    queue = queue && queue.add ? queue : new FnQueue();
+    queue.add(fns, options);
+    return queue;
+  }
 
-	// 添加订阅
-	on(id, fns) {
-		// 判断参数类型
-		if (!/function|array/.test(Utils.type(fns))) {
-			return false;
-		}
+  // 添加订阅
+  on(id = '', fns = [], options = {}) {
+    // 判断参数类型
+    if (!/function|array/.test(utils.type(fns))) {
+      return this;
+    }
 
-		if (!this.stack[id]) {
-			this.stack[id] = {};
-		}
+    if (!this.stack[id]) {
+      this.stack[id] = {};
+    }
 
-		this.stack[id].queue = this.add(this.stack[id].queue, fns);
+    // 生成队列
+    this.stack[id].queue = this.add(this.stack[id].queue, fns, { ...this.options, ...options });
 
-		// 如果当前订阅的事件存在于缓存，则取出缓存中存放的信息
-		if (this.cache[id]) {
-			this.stack[id].queue.fire(...this.cache[id].params);
-			// 清空对应缓存
-			this.cache[id] = {};
-		}
+    // 如果当前订阅的事件存在于缓存，则取出缓存中存放的信息
+    if (this.cache[id]) {
+      this.stack[id].queue.fire(...this.cache[id].params);
+      // 清空对应缓存
+      this.cache[id] = {};
+    }
 
-		return this;
-	}
+    return this;
+  }
 
-	// 发布事件
-	emit() {
-		// 获取订阅名称
-		const id = Array.prototype.shift.call(arguments);
+  // 发布事件
+  emit(id, ...args) {
+    // 1：事件存在则执行
+    if (this.stack[id]) {
+      this.stack[id].queue.fire(...args);
+    } else {
+      // 2：事件不存在则放入缓存列表
+      this.cache[id] = {
+        params: args
+      };
+    }
 
-		// 1：事件存在则执行
-		if (this.stack[id]) {
-			this.stack[id].queue.fire(...arguments);
-		} else {
-			// 2：事件不存在则放入缓存列表
-			this.cache[id] = {
-				params: arguments
-			};
-		}
+    return this;
+  }
 
-		return this;
-	}
-
-	// 移除订阅
-	remove(id, items) {
-		if (!this.stack[id]) {
-			return false;
-		}
-		this.stack[id].queue.remove(items);
-		return this;
-	}
+  // 移除订阅
+  remove(id, items) {
+    if (this.stack[id]) {
+      this.stack[id].queue.remove(items);
+    }
+    return this;
+  }
 }
+
+export { FnQueue, Signal };
--- webpack.config.js
+++ webpack.config.js
@@ -6,115 +6,90 @@ const webpack = require('webpack');
 
 /*------------------------------------------------ Plugins -----------------------------------------------------*/
 
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin');
 
 /*------------------------------------------------ Common Config -----------------------------------------------------*/
 
-const ROOT_PATH = path.join(__dirname); // 配置路径
-const SOURCE_PATH = path.join(__dirname, 'source');
-const DIST_PATH = path.join(ROOT_PATH, '__build__');
+const ROOT_PATH = path.resolve(__dirname); // 配置路径
+const SOURCE_PATH = path.resolve(__dirname, 'src');
+const DIST_PATH = path.resolve(ROOT_PATH, 'dist');
 
 const FILE_MATCHER = /([^\/]+)\.(js)$/i;
+const FILE_POOL = ['evt'];
 
 /*------------------------------------------------ entry -----------------------------------------------------*/
 
-const entryObject = (function () {
-	let files = glob.sync(SOURCE_PATH + '/*'),
-		entryMap = {};
-	if (files.length) {
-		files.forEach(file => {
-			const name = file.match(FILE_MATCHER);
-			name ? (entryMap[name[1]] = [file]) : '';
-		});
-	}
-	return entryMap;
+const entryObject = (function() {
+  let files = glob.sync(SOURCE_PATH + '/*');
+  let entryMap = {};
+  if (files.length) {
+    files.forEach(file => {
+      const name = file.match(FILE_MATCHER);
+      if (name && FILE_POOL.includes(name[1])) {
+        entryMap[name[1]] = [file];
+      }
+    });
+  }
+  return entryMap;
 })();
 
 let config = [];
 
 function generateConfig(name) {
-	const uglify = name.indexOf('min') > -1;
-	const insideConfig = {
-		entry: entryObject,
-		output: {
-			path: DIST_PATH,
-			filename: `${name}.js`,
-			chunkFilename: '[id].chunk.js',
-			publicPath: '/__build__/',
-			libraryTarget: 'umd',
-			library: 'evt'
-		},
+  const uglify = name.indexOf('min') > -1;
+  const insideConfig = {
+    entry: entryObject,
+    output: {
+      path: DIST_PATH,
+      filename: `${name}.js`,
+      publicPath: '/dist/',
+      libraryTarget: 'umd',
+      library: 'evt'
+    },
 
-		// 模块编译配置
-		module: {
-			rules: [{
-				test: /\.js?$/i,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						babelrc: false,
-						presets: [
-							[
-								'env',
-								{
-									target: {
-										browsers: ['last 2 versions']
-									},
-									modules: false,
-									useBuiltIns: true,
-									debug: false
-								}
-							],
-							'stage-0'
-						],
-						plugins: [
-							[
-								'transform-runtime',
-								{
-									helpers: false,
-									polyfill: true,
-									regenerator: true,
-									moduleName: 'babel-runtime'
-								}
-							]
-						]
-					}
-				},
-				include: SOURCE_PATH
-			}]
-		},
-		plugins: [
-			new webpack.DefinePlugin({
-				'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-			}),
-			new webpack.optimize.ModuleConcatenationPlugin(),
-		]
-	};
-	if (uglify) {
-		insideConfig.plugins.push(
-			// 利用多线程提升打包速度
-			new UglifyjsWebpackPlugin({
-				uglifyOptions: {
-					ie8: false,
-					ecma: 5,
-					mangle: true,
-					minimize: true,
-					output: {
-						comments: false
-					},
-					compress: true
-				},
-				sourceMap: false,
-				cache: false,
-				parallel: OS.cpus().length
-			})
-		);
-	}
-	return insideConfig;
+    // 模块编译配置
+    module: {
+      rules: [
+        {
+          test: /\.js$/i,
+          use: {
+            loader: 'babel-loader'
+          },
+          include: SOURCE_PATH,
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist'], {
+        root: ROOT_PATH,
+        verbose: false
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      })
+    ]
+  };
+  if (uglify) {
+    insideConfig.plugins.push(
+      // 利用多线程提升打包速度
+      new UglifyjsWebpackPlugin({
+        uglifyOptions: {
+          minimize: true,
+          compress: true
+        }
+      })
+    );
+  }
+  return insideConfig;
 }
 
-['evt', 'evt.min'].forEach(function (key) {
-	config.push(generateConfig(key));
+['evt', 'evt.min'].forEach(function(key) {
+  config.push(generateConfig(key));
 });
 
 // 配置
